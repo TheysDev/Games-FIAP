8db0fe4c7a81047f06dba7239b4d4664abfd226c
theus <theys.dev@gmail.com>
2025-04-20 09:48:55 -0300

    Salvando dados com Hibernate

--- src/main/java/br/com/fiap/games/GameApp.java
+++ src/main/java/br/com/fiap/games/GameApp.java
@@ -1,27 +1,27 @@
 package br.com.fiap.games;
 
+import br.com.fiap.games.dao.GameDao;
 import br.com.fiap.games.model.Game;
+import br.com.fiap.games.utils.Conexao;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 import java.time.LocalDate;
 
 public class GameApp {
 
     public static void main(String[] args) {
         Game game1 = new Game();
-        game1.setTitulo("Mega Man 1");
-        game1.setCategoria("Plataforma");
-        game1.setDataLancamentos(LocalDate.of(1987,12,1));
+        game1.setTitulo("Batletoads");
+        game1.setCategoria("Luta");
+        game1.setDataLancamentos(LocalDate.of(1992,8,1));
         game1.setFinalizado(true);
-        game1.setProdutora("Capcom");
-        game1.setValor(128.0);
+        game1.setProdutora("TradeWest");
+        game1.setValor(99.89);
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("games");
-        EntityManager em = emf.createEntityManager();
+        EntityManager em = Conexao.getEntityManager();
+        GameDao gameDao = new GameDao(em);
 
         em.getTransaction().begin();
-        em.persist(game1);
+        gameDao.salvar(game1);
         em.getTransaction().commit();
         em.close();
     }
